2385945533aabbbb386b6cb2f2cb8c403bc69384
ArloReilly <ajreilly@ucsc.edu>
2022-04-19 17:14:29 -0700

    changes?

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
