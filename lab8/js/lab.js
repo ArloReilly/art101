c16766d2ed68b5b3f3f4f973123c9ef9337b59a0
ArloReilly <ajreilly@ucsc.edu>
2022-05-03 08:40:51 -0700

    lab8js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -5,22 +5,25 @@
  * License:   Public Domain
  **/
 
- // Define Variables
- make = "Toyota";
- model = "Rav4";
- color = "Silver";
- year = "2020";
- name = "Betsy";
- car = "leased";
+ function isEven(x){
 
- // calculate
- age = 2022 - year;
+ }
+ function isEven(x){
+     return (x % 2 == 0);
+ }
+ //test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
 
- // output
- document.writeln("Make: " + make + "<br>");
- document.writeln("Model: " + model + "<br>");
- document.writeln("Color: " + color + "<br>");
- document.writeln("Year: " + year + "<br>");
- document.writeln("Name: " + name + "<br>");
- document.writeln("Car: " + car + "<br>");
- document.writeln("Age: " + age + "years<br>");
+ array = [200, 5, 70, 134, 789, 1000, 600]
+ console.log("My array", array);
+
+var result = array.map(isEven);
+//should return  [true, false, true, true, false, true, true]
+console.log("Test of evenness of array",result);
+
+var result = array.map(function(x){
+  return x ** 0.2;
+})
+//should return [2.8853998118144273, 1.379729661461215, 2.3389428374280206, 2.6633053388644834, 3.7967797155355707, 3.9810717055349727, 3.5944318187380233]
+console.log("new array:",result);
