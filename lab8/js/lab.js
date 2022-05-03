50366dc4d2dc0852508ac10e741842c2b2865e28
ArloReilly <ajreilly@ucsc.edu>
2022-05-03 01:06:45 -0700

    fix

---
+++ lab8/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    Arlo
+ * Created:   11.05.2009
+ *
+ * License:   Public Domain
+ **/
+
+ // Define Variables
+ make = "Toyota";
+ model = "Rav4";
+ color = "Silver";
+ year = "2020";
+ name = "Betsy";
+ car = "leased";
+
+ // calculate
+ age = 2022 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Name: " + name + "<br>");
+ document.writeln("Car: " + car + "<br>");
+ document.writeln("Age: " + age + "years<br>");
