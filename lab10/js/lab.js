dc68e22c37b491c67fb60fa824c35698fb1d5a0e
ArloReilly <ajreilly@ucsc.edu>
2022-05-11 10:05:31 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Arlo
+ * Created:   11.05.2009
+ *
+ * License:   Public Domain
+ **/
+
+ //find the element
+ var button = document,getElementById("my-button");
+ console.log("Button", buttonEl);
+ //Create an event handler within the JS file that listens for a button click.
+ bottonEl.oddEventlistener("click", function(){
+
+ //When the button is pushed, prompt the user to input their name with the prompt() function.
+ var name = window.promt("what is ypout name?");
+ console.log ("name:"name);
+ //Use this input to change the text from ‘Hello, World!’ to ‘Hello’ + the user’s name
+
+
+
+
+ });
