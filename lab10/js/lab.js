8ca143eb2fda673b6a46f154b8ca51201dabb18f
ArloReilly <ajreilly@ucsc.edu>
2022-05-13 14:36:25 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,43 +5,42 @@
  * License:   Public Domain
  **/
 
+document.getElement("my-button").addEventListener("click", sortUserName);
+function sortUserName() {
+  document.getElementById("output").inner = Date();
+}
 
- var buttonEl = document.getElementById("my-button");
- console.log("Button:", buttonEl);
+//lab7
 
- // Create an event handler within the JS file that listens for a button click.
- buttonEl.addEventListener("click", function(){
- 	console.log("Click");
- 	// prompt the user to input their name with the prompt() function.
- 	//var name = window.prompt("What is your name?");
-   var inputEl = document.getElementById("my-input");
-   var name = inputEl.value;
-   console.log("name:", name);
- 	// Use this input to change the text from ‘Heyyyyooo!’ to ‘yo yo yo’ + the user’s name
-   // Find the element
- 	var goodbyeWorld = document.getElementById("title");
-   console.log("title:", goodbyeWorld);
-   goodbyeWorld.innerHTML = "Yo yo yo, " + name;
+function sortUserName() {
+  var userName = window.prompt("Id just love to know and fix your name")
+  consoel.log("userName =", userName);
 
-   //sortUserName - a function that takes user input and sorts the letters of their name
-   function sortUserName() {
-      var userName = window.prompt("Whats your name?");
-      console.log("userName =", userName);
-      //split string to array
-      var nameAray = userName.split('');
-      console.log("nameArray =", nameArray);
-      //sort the array
-      var nameArraySort = nameArray.sort();
-      console.log("nameArraySort =", nameArraySort);
-      //join array back to a string
-      var nameSorted = nameArraySort.join('');
-      console.log("nameSorted =", nameSorted);
-      // username.toLower().split('').sort().join("")
-      return nameSorted;
-   }
+  //split string to array
 
-  // output
-  document.writeln("Oh hey, I've fixed your name: ",
-    sortUserName(), "</br>");
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
 
-});
+  //sort array
+
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort=", nameArraySort);
+
+  //join array and string back together
+
+  var nameSorted = nameArraySorted.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//Output
+
+document.writeln("Yo yo yo! Well looky I made your name better, "
+  sortUserName(), "</br>");
+
+
+
+
+
+
+ 
