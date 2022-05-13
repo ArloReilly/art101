e97f9718134faaad4ba5674d7e1584ec0bb0baa5
ArloReilly <ajreilly@ucsc.edu>
2022-05-12 17:52:54 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -23,4 +23,25 @@
    console.log("title:", goodbyeWorld);
    goodbyeWorld.innerHTML = "Yo yo yo, " + name;
 
+   //sortUserName - a function that takes user input and sorts the letters of their name
+   function sortUserName() {
+      var userName = window.promt("Whats your name?");
+      console.log("userName =", userName);
+      //split string to array
+      var nameAray = userName.split('');
+      console.log("nameArray =", nameArray);
+      //sort the array
+      var nameArraySort = nameArray.sort();
+      console.log("nameArraySort =", nameArraySort);
+      //join array back to a string
+      var nameSorted = nameArraySort.join('');
+      console.log("nameSorted =", nameSorted);
+      // username.toLower().split('').sort().join("")
+      return nameSorted;
+   }
+
+  // output
+  document.writeln("Oh hey, I've fixed your name: ",
+    sortUserName(), "</br>");
+
 });
