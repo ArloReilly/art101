0abd5e9d9a6e4d84ebdf813be77f08800d2a177c
ArloReilly <ajreilly@ucsc.edu>
2022-05-16 10:25:33 -0700

    lab12

---
+++ lab12/js/lab.js
@@ -0,0 +1,31 @@
+/**
+ * Author:    Arlo with refrences from Wes Modes
+ * Created:   11.05.2009
+ *
+ * License:   Public Domain
+ **/
+ //Return Gryffindor, Slytherin, Ravenclaw, Hufflepuff
+ //sort depending on length
+ function sortingHat (str){
+   len = str.length;
+   mod = len % 4;
+   if (mod == 0){
+     return "Gryffindor"
+   }
+   else if (mod == 1){
+     return "Ravenclaw"
+   }
+   else if (mod == 2){
+     return "Slytherin"
+   }
+   else if (mod == 3){
+     return "Hufflepuff"
+   }
+ }
+ var myButton = document.getElementById("button");
+ myButton.addEventListener("click", function() {
+   var name = document.getElementById("input").value;
+   var house = sortingHat(name);
+   newText = "<p>Congradulations you have been sorted into house" = house = "</p>";
+   document.getElementById ("output").innerHTML = newText;
+ })
