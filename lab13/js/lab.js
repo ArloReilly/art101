ec46c6a5e5321d0794fcddb9675aa4c7ab0e9df4
ArloReilly <ajreilly@ucsc.edu>
2022-05-23 09:47:25 -0700

    lab13js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,28 +4,14 @@
  *
  * License:   Public Domain
  **/
- //Return Gryffindor, Slytherin, Ravenclaw, Hufflepuff
- //sort depending on length
- function sortingHat (str){
-   len = str.length;
-   mod = len % 4;
-   if (mod == 0){
-     return "Gryffindor"
-   }
-   else if (mod == 1){
-     return "Ravenclaw"
-   }
-   else if (mod == 2){
-     return "Slytherin"
-   }
-   else if (mod == 3){
-     return "Hufflepuff"
-   }
- }
- var myButton = document.getElementById("button");
- myButton.addEventListener("click", function() {
-   var name = document.getElementById("input").value;
-   var house = sortingHat(name);
-   newText = "<p>Congratulations you have been sorted into house " + house + "</p>";
-   document.getElementById ("output").innerHTML = newText;
- })
+ for (i=1; i <= 100; i++) {
+    if (i%15 == 0) {
+        console.log("FizzBuzz");
+    } else if (i%3 == 0) {
+        console.log("Fizz");
+    } else if (i%5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
