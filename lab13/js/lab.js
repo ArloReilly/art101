61b19f3a17095ec939450d7018dc3ba6dbbe63ef
ArloReilly <ajreilly@ucsc.edu>
2022-05-25 15:27:38 -0700

    lab13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,14 +4,38 @@
  *
  * License:   Public Domain
  **/
- for (i=1; i <= 100; i++) {
-    if (i%15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i%3 == 0) {
-        console.log("Fizz");
-    } else if (i%5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
+//given a number and an object that looks like this:
+// 3: "snap", 5: "crackle" 7: "Pop"
+//loops over the numbers and outputs the number and the matching text for factors
+function snapCracklePop(maxNums, factorObj){
+  //iterate ver all of out numbers
+  for (var num=0; num<maxNums; num++)
 }
+//reset output string
+var outputStr = "";
+//iterate over the factors we got from HTML
+for (var factor in factorObj){
+  //check and see if num is a multiple of factor
+  if (num % factor == 0){
+
+//if yes than add text to output string
+outputStr += factorObj[factor];
+}
+}
+//now if we have words in outputStr, format is like this, "SnapCrackle!"
+if (outputStr) {
+  outputStr = " - " + outputStr + "!";
+}
+outputToPage(num.toString() + outputStr)
+}
+function reportError(str){
+  outputEl.innerHTML = "<div class= 'error'>" + str + "</div";
+}
+
+document.getElementById("submit").addEventListner("click", function() {
+  var max = document.getElementById("max").value;
+  console.log("max", max)
+  if (! max) {
+    reportError("You gotta gimme a max");
+  }
+})
