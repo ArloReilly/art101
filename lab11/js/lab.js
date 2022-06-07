2db5c36d3268e2795e3415e15bd63b8514fef3ba
ArloReilly <ajreilly@ucsc.edu>
2022-06-06 18:24:06 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,8 +4,18 @@
  *
  * License:   Public Domain
  **/
- $("#butt1").click(function(){
- 	$("#boat").toggleClass("selected");
-   var name = $("#my-input").val();
-   $("h1").html("Hi, " + name);
- })
+ var butt1 = $("#challenges").append("<button> Press Me! </button>");
+ //add click Toggle
+ $(butt1).click(function(){
+   $("#p1").toggleClass("selected");
+ });
+ //add a button bellow the problem div
+ var butt2 = $("#problems").append("<button> Heres Another Butt! </button>");
+ $(butt2).click(function(){
+   $("#p2").toggleClass("selected2");
+ });
+ //add a button bellow the results div
+ var butt3 = $("#problems").append("<button> One More Time! </button>");
+ $(butt3).click(function(){
+   $("#p3").toggleClass("selected3");
+ });
